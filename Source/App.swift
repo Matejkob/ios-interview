806227c94d425fab9a41842ea41227b75376f3f7
Matthew <86073556+MatthewXk@users.noreply.github.com>
2024-07-08 08:19:55 +0200

    Add content view

--- Source/App.swift
+++ Source/App.swift
@@ -5,11 +5,45 @@ import SwiftUI
 // MARK: - Main View of the App
 
 struct ContentView: View {
+    @State var count: Int
+
     var body: some View {
-        Text("Hi!")
+        // let _ = Self._printChanges()
+        Form {
+            Section {
+                Text(self.count.description)
+                Button("Decrement") { decrementButtonTapped() }
+                Button("Increment") { incrementButtonTapped() }
+            } header: {
+                Text("Counter")
+            }
+        }
+    }
+
+    func decrementButtonTapped() {
+        count -= 1
+    }
+
+    func incrementButtonTapped() {
+        count += 1
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(count: 0)
 }
+
+/*
+ var body: some View {
+ // let _ = Self._printChanges()
+ Form {
+ Section {
+ Text(self.count.description)
+ Button("Decrement") { decrementButtonTapped() }
+ Button("Increment") { incrementButtonTapped() }
+ } header: {
+ Text("Counter")
+ }
+ }
+ }
+ */
