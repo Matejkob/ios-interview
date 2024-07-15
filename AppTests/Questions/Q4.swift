a2fce6cd3edacb3bd728e68974d54d532aac232b
LukaszWojtarowicz <lukasz.wojtarowicz@interia.pl>
2024-07-15 18:58:08 +0200

    Q4 added: PassthroughSubject vs CurrentValueSubject

---
+++ AppTests/Questions/Q4.swift
@@ -0,0 +1,56 @@
+//
+//  Q4.swift
+//  AppTests
+//
+//  Created by Łukasz Wojtarowicz on 15/07/2024.
+//
+import XCTest
+import Combine
+
+final class Q4: XCTestCase {
+    let names = CurrentValueSubject<[String], Never>(["Kevin"])
+    let newNameEntered = PassthroughSubject<String,Never>()
+    var subscriptions = Set<AnyCancellable>()
+    
+    func test_passtroughOnSubscribe() {
+        newNameEntered.sink {
+            print($0)
+        } receiveValue: { value in
+            // what kind of output will generate here print($0)?
+        }.store
+    }
+    
+    func test_passtroughNewValues() {
+        //what will You do to adjust that test, to satisfy assert?
+        
+        newNameEntered.sink {
+            print($0)
+        } receiveValue: { value in
+           XCTAssertEqual(value, "Rambo")
+        }.store
+    }
+    
+    func test_currentValueSubjectOnSubscribe() {
+        names.sink {
+            print($0)
+        } receiveValue: { value in
+            // what kind of output will generate here print($0)?
+        }.store
+    }
+    
+    func test_currentValueSubjectNewValues() {
+        //what will You do to adjust that test, to satisfy assert?
+        newNameEntered.sink { completion in
+
+        } receiveValue: { value in
+            
+        }.store
+        
+        names.sink {
+            print($0)
+        } receiveValue: { values in
+            XCTAssertEqual(values, ["Kevin", "Susan"])
+        }
+        
+    }
+}
